5ce81317234ca6430ebc9acc57a19c08fdedce86
dhanushkumar <96973340+dhanushdk27@users.noreply.github.com>
2022-01-09 15:38:14 +0530

    Add files via upload
    df

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <img src="https://res.cloudinary.com/urbanclap/image/upload/images/growth/home-screen/1631097450980-d2de38.png"/>
+      
+      <div className="span1">
+        <a href="/chennai" target="_blank" rel="nooneper" itemprop="item">
+        <span>Home /</span>
+        </a>
+      <a href="/chennai" target="_blank" rel="nooneper" itemprop="item">
+        <span> Chennai</span>
+        </a>
+        
+        </div>
+        <h1 className="firsth1" >Home servies, on demond.</h1>
+        
+    </div>
+    
+          <select className="select1" placeholder="9/891,89th,street" ></select>
+          <input type="text" placeholder="Search for servies"></input>
+          <div className="lightshadow"></div>
+          <div className="span2">
+        <a href="/chennai" target="_blank" rel="nooneper" itemprop="item">
+        <span className="main">Massage for Men's</span>
+        </a>
+      <a href="/chennai" target="_blank" rel="nooneper" itemprop="item">
+        <span  className="main1">  Home Painting,</span>
+        </a>
+        <a href="/chennai" target="_blank" rel="nooneper" itemprop="item">
+        <span  className="main2"> Electranics  Etc</span>
+        </a>
+        </div>
+    </>
+  );
+}
+
+export default App;
